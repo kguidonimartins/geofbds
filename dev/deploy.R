f6299879057b7218d5d5e3a94ed886fd69effb3d
Karlo Guidoni <kguidonimartins@gmail.com>
2026-09-14 22:48:51 -0300

    fix: deploy with the renv project library

--- dev/deploy.R
+++ dev/deploy.R
@@ -38,6 +38,9 @@ install_if_needed("rsconnect", "1.10.0")
 install_if_needed("dotenv")
 
 root <- normalizePath(".")
+invisible(loadNamespace("rsconnect"))
+invisible(loadNamespace("dotenv"))
+source(file.path(root, "renv", "activate.R"))
 
 env_file <- file.path(root, ".env")
 
@@ -74,45 +77,15 @@ if (!file.copy(lockfile, app_lockfile, overwrite = FALSE)) {
 }
 
 tryCatch(
-  {
-    lock <- jsonlite::read_json(app_lockfile, simplifyVector = FALSE)
-    package <- lock$Packages$geofbds
-    remote_fields <- c(
-      package$RemoteUsername,
-      package$RemoteRepo,
-      package$RemoteRef,
-      package$RemoteSha
-    )
-
-    if (is.null(package) || any(!nzchar(remote_fields))) {
-      stop(
-        "O renv.lock nao contem a origem GitHub completa de geofbds.",
-        call. = FALSE
-      )
-    }
-
-    package$GithubUsername <- package$RemoteUsername
-    package$GithubRepo <- package$RemoteRepo
-    package$GithubRef <- package$RemoteRef
-    package$GithubSHA1 <- package$RemoteSha
-    lock$Packages$geofbds <- package
-    jsonlite::write_json(
-      lock,
-      app_lockfile,
-      auto_unbox = TRUE,
-      pretty = TRUE
-    )
-
-    rsconnect::deployApp(
-      appName = "geofbds",
-      appDir = app_dir,
-      account = "kguidonimartins",
-      upload = TRUE,
-      launch.browser = TRUE,
-      forceUpdate = TRUE,
-      logLevel = "verbose",
-      lint = TRUE
-    )
-  },
+  rsconnect::deployApp(
+    appName = "geofbds",
+    appDir = app_dir,
+    account = "kguidonimartins",
+    upload = TRUE,
+    launch.browser = TRUE,
+    forceUpdate = TRUE,
+    logLevel = "verbose",
+    lint = TRUE
+  ),
   finally = unlink(app_lockfile)
 )
